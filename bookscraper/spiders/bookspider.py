7db7e67876e8de1d305abf7a3c082ea7e8eab3d4
tamjidzihan <tamjidzihan@gmail.com>
2023-10-16 13:57:06 +0600

    office 2023-10-16

--- bookscraper/spiders/bookspider.py
+++ bookscraper/spiders/bookspider.py
@@ -8,4 +8,14 @@ class BookspiderSpider(scrapy.Spider):
     start_urls = ["https://books.toscrape.com"]
 
     def parse(self, response):
-        pass
+        books = response.css('article.product_pod')
+        for book in books:
+            yield{
+                'name': book.css('h3 a::text').get(),
+                'price':book.css('.product_price .price_color::text').get(),
+                'link':book.css('h3 a').attrib['href']
+            }
+        next_page = response.css('li.next a::attr(href)').get()
+        if next_page is not None:
+            next_page_url = 'https://books.toscrape.com/' + next_page
+            yield response.follow(next_page_url, callback = self.parse)
