53adf9f73d79eb0339d2c0b0a404acacde34294a
tamjidzihan <tamjidzihan@gmail.com>
2023-10-17 20:44:39 +0600

    office 2023-10-17

--- bookscraper/spiders/bookspider.py
+++ bookscraper/spiders/bookspider.py
@@ -15,6 +15,7 @@ class BookspiderSpider(scrapy.Spider):
                 next_book_url = 'https://books.toscrape.com/' + next_book
             else:
                 next_book_url = 'https://books.toscrape.com/catalogue/' + next_book
+            print(next_book_url)
             yield response.follow(next_book_url, callback = self.parse_books_url)
 
         next_page = response.css('li.next a::attr(href)').get()
@@ -26,4 +27,13 @@ class BookspiderSpider(scrapy.Spider):
             yield response.follow(next_page_url, callback = self.parse)
 
     def parse_books_url(self,response):
-        book_detail = response.css
+        tables = response.css('table tr')
+        yield{
+            'name':response.css('.product_main h1::text').get(),
+            'product_type': tables[1].css('td::text').get(),
+            'price':response.css('p.price_color::text').get(),
+            'price_excl_tax':tables[2].css('td::text').get(),
+            'price_incl_tax':tables[3].css('td::text').get(),
+            'Availability': tables[5].css('td::text').get(),
+            'catagory':response.xpath('//*[@id="default"]/div/div/ul/li[3]/a/text()').get()
+        }
